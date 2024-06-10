65498406cf01db18f20fc050c475df389a1d46e8
pavomit <spiderocta@proton.me>
2024-06-10 03:33:28 +0300

    added player scores

--- main.cpp
+++ main.cpp
@@ -13,12 +13,10 @@ int main()
 	// Left Player
 	sf::RectangleShape leftPlayer(sf::Vector2f(20, 100));
 	sf::Vector2f playerSize = leftPlayer.getSize();
-	leftPlayer.setFillColor(sf::Color(255, 229, 180));
 	leftPlayer.setPosition(-playerSize.x / 2, windowSize.y / 2 - playerSize.y / 2);
 
 	//Right Player
 	sf::RectangleShape rightPlayer(sf::Vector2f(20, 100));
-	rightPlayer.setFillColor(sf::Color(255, 229, 180));
 	rightPlayer.setPosition(windowSize.x - playerSize.x / 2, windowSize.y / 2 - playerSize.y / 2);
 
 
@@ -29,9 +27,34 @@ int main()
 	sf::Vector2f ballVelocity(8, 2);
 
 	// ++++++++++++++++++++++++ screen line +++++++++++++++++++++++++++++
-	sf::RectangleShape line(sf::Vector2f(2,windowSize.y));
+	sf::RectangleShape line(sf::Vector2f(2, windowSize.y));
 	line.setPosition(windowSize.x / 2 - 10, 0);
-	line.setFillColor(sf::Color(255, 229, 150));
+	line.setFillColor(sf::Color(128, 128, 128, 140));
+
+	//++++++++++++++++++ SCore ++++++++++++++++++++++++++++++++++
+	sf::Font font;
+	if (!font.loadFromFile("../assets/fonts/font.ttf")) {
+		std::cerr << "Error loading font from file @ assets/fonts/font.ttf\n";
+		return 1; // Exit the program if the font fails to load
+	}
+
+	sf::Text lScore, rScore;
+	int leftVal{ 0 }, rightVal{ 0 };
+
+	lScore.setFont(font);
+	rScore.setFont(font);
+
+	lScore.setCharacterSize(20);
+	rScore.setCharacterSize(20);
+
+	lScore.setString(std::to_string(leftVal));
+	rScore.setString(std::to_string(rightVal));
+
+	float totalScoreWidth = lScore.getGlobalBounds().width + rScore.getGlobalBounds().width;
+	float x = (windowSize.x - totalScoreWidth) / 2.0f;
+
+	rScore.setPosition(x + lScore.getGlobalBounds().width + 10, 20);
+	lScore.setPosition(x - 30, 20); // Adjust spacing as needed
 
 	//++++++++++++++++++++++++  game loop +++++++++++++++++++++++++++
 	while (window.isOpen()) {
@@ -44,16 +67,16 @@ int main()
 
 			//basic movement for both players (W | S for Left Player - up | Down for right player)
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-				rightPlayer.move(sf::Vector2f(0, -20));
+				rightPlayer.move(sf::Vector2f(0, -25));
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-				rightPlayer.move(sf::Vector2f(0, 20));
+				rightPlayer.move(sf::Vector2f(0, 25));
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-				leftPlayer.move(sf::Vector2f(0, -20));
+				leftPlayer.move(sf::Vector2f(0, -25));
 			}
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-				leftPlayer.move(sf::Vector2f(0, 20));
+				leftPlayer.move(sf::Vector2f(0, 25));
 			}
 
 		}
@@ -92,8 +115,18 @@ int main()
 
 		// Check for window collision
 		if (ball.getPosition().x < 0 || ball.getPosition().x + ball.getGlobalBounds().width > windowSize.x) {
+
+			//update score
+			if (ball.getPosition().x < 0) {
+				rightVal++;
+				rScore.setString(std::to_string(rightVal));
+			}
+			else if (ball.getPosition().x + ball.getGlobalBounds().width > windowSize.x) {
+				leftVal++;
+				lScore.setString(std::to_string(leftVal));
+			}
+
 			// Reverse horizontal velocity
-			//ballVelocity.x = -ballVelocity.x;
 			ball.setPosition(sf::Vector2f(window.getSize().x / 2, window.getSize().y / 2));
 		}
 		if (ball.getPosition().y < 0 || ball.getPosition().y + ball.getGlobalBounds().height > windowSize.y) {
@@ -101,12 +134,13 @@ int main()
 			ballVelocity.y = -ballVelocity.y;
 		}
 
-
 		window.clear();
 		window.draw(leftPlayer);
 		window.draw(rightPlayer);
 		window.draw(line);
 		window.draw(ball);
+		window.draw(lScore);
+		window.draw(rScore);
 		window.display();
 	}
 
